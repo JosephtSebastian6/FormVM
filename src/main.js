9ff89286e955e390e428d1bbc59fe39ae2e00c2a
JosephtSebastian6 <sebastiangonzalez3216@gmail.com>
2025-04-02 21:17:45 -0500

    servidor corriendo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { setupFormHandler } from "./js/modules/formHandler";
+import  setupFormHandler from "./js/modules/formHandler";
 
 document.addEventListener("DOMContentLoaded", () => {
   setupFormHandler();
